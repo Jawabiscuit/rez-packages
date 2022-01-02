94075187752ad4baadf1fc041912f650ddb9e8ee
javrin <jawabiscuit@users.noreply.github.com>
2022-01-02 10:54:54 -0500

    Emacs 27 initial commit

---
+++ emacs/27/package.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+name = "emacs"
+
+version = "27"
+
+authors = [
+    "gnu",
+]
+
+description = \
+    """
+    Emacs is the extensible, customizable, self-documenting real-time display editor.
+    """
+
+help = "https://www.emacswiki.org/emacs/GccEmacs"
+
+tools = [
+    "emacs",
+    "emacsclient",
+    "emacsclientw",
+]
+
+variants = [
+    ["platform-windows", "arch-AMD64"],
+]
+
+requires = []
+
+private_build_requires = [
+    "python-2+<3",
+]
+
+uuid = "packages.{}-{}".format(name, version)
+
+
+@early()
+def build_command():
+    import os
+
+    if os.name == "nt":
+        command = 'pwsh -File "{0}"'
+        prefix = "%REZ_BUILD_SOURCE_PATH%"
+        script = "build.ps1"
+        return command.format(os.path.join(prefix, script))
+
+
+def commands():
+    import os
+
+    if os.name == "nt":
+        env.HOME.prepend(os.environ["USERPROFILE"])
+        env.PATH.append(os.path.join("{root}", "bin"))
